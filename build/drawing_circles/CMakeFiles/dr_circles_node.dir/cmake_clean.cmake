8b0f65918ad86ad359b09bfaaa731c6c4b873c9b
LukaszMajchrzak3 <133999034+LukaszMajchrzak3@users.noreply.github.com>
2024-04-01 11:22:27 +0200

    Add files via upload

---
+++ build/drawing_circles/CMakeFiles/dr_circles_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dr_circles_node.dir/src/dr_circles_node.cpp.o"
+  "CMakeFiles/dr_circles_node.dir/src/dr_circles_node.cpp.o.d"
+  "dr_circles_node"
+  "dr_circles_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dr_circles_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
